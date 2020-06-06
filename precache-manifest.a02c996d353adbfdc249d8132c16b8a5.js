97c6a5edcd52dac55066037c16394e636b0431b2
Rafał Białek <bialekrafal94@gmail.com>
2020-06-06 03:33:06 +0200

    Updates

---
+++ precache-manifest.a02c996d353adbfdc249d8132c16b8a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96068643284db004511717ccca6d06c6",
+    "url": "/Guess-App/index.html"
+  },
+  {
+    "revision": "32b99b3d3109808920f6",
+    "url": "/Guess-App/static/css/2.3ffaa857.chunk.css"
+  },
+  {
+    "revision": "0860e8f157b0163af6af",
+    "url": "/Guess-App/static/css/main.40f9afd5.chunk.css"
+  },
+  {
+    "revision": "32b99b3d3109808920f6",
+    "url": "/Guess-App/static/js/2.94496f4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Guess-App/static/js/2.94496f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0860e8f157b0163af6af",
+    "url": "/Guess-App/static/js/main.022ad00a.chunk.js"
+  },
+  {
+    "revision": "d5c728ce65800797379f",
+    "url": "/Guess-App/static/js/runtime-main.f5b6c02a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Guess-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Guess-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Guess-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Guess-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Guess-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
